ab223a84cf62fe37e59dbea92b91326fca7f5053
naby <naby@pop-os.localdomain>
2022-05-09 15:20:56 +0200

    add spinner

--- client/src/components/GameModal.vue
+++ client/src/components/GameModal.vue
@@ -22,57 +22,58 @@
                          leave="ease-in duration-200"
                          leave-from="opacity-100 translate-y-0 sm:scale-100"
                          leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-          <div class="relative inline-block align-bottom bg-[#303234] rounded-3xl text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-[90vw]">
-            <div class="pb-4 text-white">
-              <div class="text-md sm:text-lg">
-                <div class="absolute">
-                  <button @click="$emit('closeModal')" class="relative left-[82vw] lg:left-[85vw] xl:left-[87vw] top-3">
-                    <font-awesome-icon :icon="['fas', 'xmark']" size="3x" class="icon-gradient" />
-                  </button>
-                </div>
-                <div class="max-h-[25vh] sm:max-h-[35vh] overflow-hidden rounded-3xl">
-                  <img class="w-full" v-if="state.game.artworks" :src="state.game.artworks[0].url.replace('t_thumb', 't_1080p')" :alt="state.game.id"/>
-                  <div class="h-[35vh] bg-gray-600 flex items-center justify-center" v-else>Aucun artwork disponible</div>
-                </div>
+          <spinner-loading v-if="state.loading"></spinner-loading>
+          <div v-else class="relative inline-block align-bottom bg-[#303234] rounded-3xl text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-[90vw]">
+              <div class="pb-4 text-white">
+                <div class="text-md sm:text-lg">
+                  <div class="absolute">
+                    <button @click="$emit('closeModal')" class="relative left-[82vw] lg:left-[85vw] xl:left-[87vw] top-3">
+                      <font-awesome-icon :icon="['fas', 'xmark']" size="3x" class="icon-gradient" />
+                    </button>
+                  </div>
+                  <div class="max-h-[25vh] sm:max-h-[35vh] overflow-hidden rounded-3xl">
+                    <img class="w-full" v-if="state.game.artworks" :src="state.game.artworks[0].url.replace('t_thumb', 't_1080p')" :alt="state.game.id"/>
+                    <div class="h-[35vh] bg-gray-600 flex items-center justify-center" v-else>Aucun artwork disponible</div>
+                  </div>
 
-                <div class="absolute">
-                  <rates-circle :rate="Math.round(state.game.total_rating*10/10)" class="relative sm:left-[77vw] left-[65vw] top-[-3rem] flex items-center justify-center text-md sm:text-lg"></rates-circle>
-                </div>
+                  <div class="absolute">
+                    <rates-circle :rate="Math.round(state.game.total_rating*10/10)" class="relative sm:left-[77vw] left-[65vw] top-[-3rem] flex items-center justify-center text-md sm:text-lg"></rates-circle>
+                  </div>
 
-                <div class="px-4 sm:px-10 mt-3">
-                  <div class="border-b border-gray-400">
-                    <div class="flex">
-                      <div class="sm:w-[55vw] lg:w-[60vw] pb-3">
-                        <DialogTitle as="h3" class="text-xl sm:text-2xl leading-6 font-medium"> {{ state.game.name }} </DialogTitle>
-                        <p>{{ formatReleasedDate() }}</p>
-                        <p v-if="state.game.involved_companies" class="italic">{{ state.game.involved_companies[0].company.name }}</p>
-                        <p><span class="text-slate-400">Support: </span>{{ formatInlineTab(state.game.platforms, ',') }}</p>
-                        <p><span class="text-slate-400">Genres: </span>{{ formatInlineTab(state.game.genres, '/') }}</p>
-                      </div>
-                      <div class="flex justify-center items-center gap-x-6 mr-2 sm:pl-[10vw]">
-                        <button>
-                          <font-awesome-icon :icon="['far', 'bookmark']" size="2x" class="icon-gradient" />
-                        </button>
-                        <button>
-                          <font-awesome-icon :icon="['far', 'heart']"  size="2x" class="icon-gradient" />
-                        </button>
+                  <div class="px-4 sm:px-10 mt-3">
+                    <div class="border-b border-gray-400">
+                      <div class="flex">
+                        <div class="sm:w-[55vw] lg:w-[60vw] pb-3">
+                          <DialogTitle as="h3" class="text-xl sm:text-2xl leading-6 font-medium"> {{ state.game.name }} </DialogTitle>
+                          <p>{{ formatReleasedDate() }}</p>
+                          <p v-if="state.game.involved_companies" class="italic">{{ state.game.involved_companies[0].company.name }}</p>
+                          <p><span class="text-slate-400">Support: </span>{{ formatInlineTab(state.game.platforms, ',') }}</p>
+                          <p><span class="text-slate-400">Genres: </span>{{ formatInlineTab(state.game.genres, '/') }}</p>
+                        </div>
+                        <div class="flex justify-center items-center gap-x-6 mr-2 sm:pl-[10vw]">
+                          <button>
+                            <font-awesome-icon :icon="['far', 'bookmark']" size="2x" class="icon-gradient" />
+                          </button>
+                          <button>
+                            <font-awesome-icon :icon="['far', 'heart']"  size="2x" class="icon-gradient" />
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div class="mt-2">
-                    <p>
-                      <span class="text-slate-400">Synopsys:</span>
-                      {{ state.game.summary }}
-                    </p>
+                    <div class="mt-2">
+                      <p>
+                        <span class="text-slate-400">Synopsys:</span>
+                        {{ state.game.summary }}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-            </div>
-            <div class="flex items-center justify-center gap-16 text-md sm:text-lg mb-5">
-              <toggle-switch title="En cours de jeu"></toggle-switch>
-              <toggle-switch title="Jeu terminé"></toggle-switch>
-            </div>
+              </div>
+              <div class="flex items-center justify-center gap-16 text-md sm:text-lg mb-5">
+                <toggle-switch title="En cours de jeu"></toggle-switch>
+                <toggle-switch title="Jeu terminé"></toggle-switch>
+              </div>
           </div>
         </TransitionChild>
       </div>
@@ -89,7 +90,7 @@ import ToggleSwitch from "@/components/ToggleSwitch.vue";
 const { getGameById } = useIgdbServices()
 import dayjs from "dayjs"
 import useIgdbServices from "@/services/igdb.services";
-import SecondaryButton from "@/components/buttons/SecondaryButton.vue";
+import SpinnerLoading from "@/components/SpinnerLoading.vue"
 
 const props = defineProps({
   open: Boolean,
@@ -100,13 +101,15 @@ defineEmits(['closeModal'])
 
 const state = reactive({
   game: {},
+  loading: false,
 })
 
 
 watch(() => props.id, (newId) => {
+  state.loading = true
   getGameById(newId as number).then((res) => {
-   state.game = res.data[0]
-    console.log(res.data)
+    state.game = res.data[0]
+    state.loading = false
   })
 })
 
@@ -128,4 +131,7 @@ const formatInlineTab = (tab:[], separator:string) => {
 #close-button{
   @apply w-full h-10 text-lg
 }
+
+
+
 </style>
---
+++ client/src/components/SpinnerLoading.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="box-border absolute top-[50%] left-[50%] w-[50px] h-[50px] mt-[-10px] ml-[-10px]
+   rounded-[50%] border-2 border-[#ccc] border-t-[#000] animate-spin" />
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
